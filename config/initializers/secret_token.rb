c1070dfba13624fdebb68f6bc152c34b8049062d
zouh <zouh74@gmail.com>
2013-11-20 22:49:11 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ISlim::Application.config.secret_key_base = '6dba98d44635c0799cd7df0011e100d2b3bfe23de7c05e7cc48bbb23abccb0d8ada3bc52472996ad2d67422433c8796027a811ba7963cccba7d9f87d1eec94b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ISlim::Application.config.session_store :cookie_store, key: '_iSlim_session'
